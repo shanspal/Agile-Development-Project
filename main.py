6afd1d8a5f81abda4131bd7cd06f3dc67482f5f6
Your Name <you@example.com>
2026-05-13 16:57:28 -0700

    fix

--- main.py
+++ main.py
@@ -3,4 +3,5 @@ from rate_my_game import create_app
 app = create_app()
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 10000))
+    app.run(debug=True, host='0.0.0.0', port=port)
